24bee8f4aa5c2d32802d72ba45c1f335b593be16
Kam3ron101 <124931254+Kam3ron101@users.noreply.github.com>
2023-04-06 19:44:28 -0400

    UserInterface.java

---
+++ UserInterface.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public interface UserInterface {
+	
+	// the start method simply begins the command loop
+	// of the corresponding user interface type (employee or manager)
+	public void start(Scanner input);
+
+}
