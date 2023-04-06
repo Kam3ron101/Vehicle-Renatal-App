bea209b3e28036f29cb8fa44657ca841d844e12a
Kam3ron101 <124931254+Kam3ron101@users.noreply.github.com>
2023-04-06 19:46:51 -0400

    TruckRates.java

---
+++ TruckRates.java
@@ -0,0 +1,18 @@
+public class TruckRates extends VehicleRates {
+	
+	public TruckRates(double daily_rate, double weekly_rate, double monthly_rate, double mileage_chrg,
+		    double daily_insur_rate) {
+				
+				super(daily_rate, weekly_rate, monthly_rate, mileage_chrg, daily_insur_rate);
+			}
+
+			
+			public String toString() {
+				
+				return " Daily Rate: " +super.getDailyRate()+ " Weekly Rate: "+super.getWeeklyRate()+ " Monthly Rate: " +super.getMonthlyRate()+
+		" Mileage Charge: "	+super.getMileageChrg()+ " Daily Insurance Rate: " +super.getDailyInsurRate();
+						
+			
+			}
+
+}
